3e0da97839780c21cf43d899969a4b555fdf4298
eli <eli.ginsburg.marcy@gmail.com>
2019-08-04 21:40:42 -0500

    worked out user input for multiple stress options

--- wordspider/spiders/stressspider.py
+++ wordspider/spiders/stressspider.py
@@ -1,11 +1,38 @@
 import scrapy
 
 
+def input_isvalid(numstr, targetlist):
+    """
+    takes a string of user input and returns true if it can be converted to
+    an int which is a valid index of targetlist
+    """
+    try:
+        numstr = int(numstr)
+    except ValueError:
+        return False
+    return numstr - 1 in range(len(targetlist))
+
+
+def clean_scraped(scrapedstring):
+    """
+    takes a scraped string and removes html tags, newlines, and tags.
+    Returns the cleanedup string
+    """
+    scrapedstring = scrapedstring.replace('<div class="word">', '')
+    scrapedstring = scrapedstring.replace('</div>', '')
+    scrapedstring = scrapedstring.replace('\n', '')
+    scrapedstring = scrapedstring.replace('\t', '')
+    scrapedstring = scrapedstring.replace('<div class="rule ">', '')
+    scrapedstring = scrapedstring.replace('<b>', '')
+    scrapedstring = scrapedstring.replace('</b>', '')
+    return scrapedstring
+
+
 class WordSpider(scrapy.Spider):
-    name = 'wordspider'
+    name = 'stressspider'
 
     def start_requests(self):
-        urls = ['https://xn----8sbhebeda0a3c5a7a.xn--p1ai/%D0%B2-%D1%81%D0%BB%D0%BE%D0%B2%D0%B5-%D0%BF%D1%82%D0%B5%D0%BD%D0%B5%D1%86/', ]
+        urls = ['https://xn----8sbhebeda0a3c5a7a.xn--p1ai/%D0%B2-%D1%81%D0%BB%D0%BE%D0%B2%D0%B5-%D0%B3%D0%BE%D1%80%D1%8B/', ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
@@ -33,6 +60,19 @@ class WordSpider(scrapy.Spider):
         Some code here to isolate the text explanation and the stressed form
         For the stresssed form, replace bold tag with colored font tag
         """
+        explanations_clean = []
+        stresses_clean = []
+        for item in explanations:
+            explanations_clean.append(clean_scraped(item))
+        for item in stresses:
+            stresses_clean.append(clean_scraped(item))
+        if len(stresses_clean) > 1:
+            for i in range(len(stresses_clean)):
+                print(f'{i + 1} - {explanations_clean[i]}')
+                print(f'{i + 1} - {stresses_clean[i]}')
+            userselect = input('It appears there is more than one option for stressing this word. Please enter the number corresponding to the appropriate stress: ')
+            while not input_isvalid(userselect, stresses_clean):
+                userselect = input('It appears you did not enter a valid choice. Please enter the number corresponding to the appropriate stress: ')
         # for verbose_stress in stresses:
         #
         #     target = verbose_example.split('<div class="v2-sentence-source">',
